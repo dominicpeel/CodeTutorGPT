be76f145ce2d1308b8349ac0f0b6d143fa8e2807
Dominic Peel <dominic@peely.org>
2023-04-10 17:06:19 +0100

    Reser user context, feedback and lessons

--- lesson.c
+++ lesson.c
@@ -2,16 +2,5 @@
 
 int main()
 {
-  int *arr = malloc(10 * sizeof(int));
-  for (int i = 0; i < 10; i++)
-  {
-    arr[i] = i;
-  }
-  for (int i = 0; i < 10; i++)
-  {
-    printf("%d\n", arr[i]);
-  }
-  free(arr);
-
   return 0;
 }
